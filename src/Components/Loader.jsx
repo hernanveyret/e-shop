a9f27995e28b35a620cb885831f5cedda0ec7e2e
hernanveyret <hernanveyret@hotmail.com>
2025-06-27 15:18:30 -0300

    agregado de funciones

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./Loader.css";
 const Loader = () => {
   return (
-    <div class="lds-roller">
-  <div class="lds-ring"></div>
+    <div className="lds-roller">
+  <div className="lds-ring"></div>
 </div>
   )
 }
