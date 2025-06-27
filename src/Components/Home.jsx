13703b2f5a702a06aff811a9fbf1d7731421ccba
hernanveyret <hernanveyret@hotmail.com>
2025-06-27 17:34:13 -0300

    add btn

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -125,6 +125,18 @@ const Home = ({ productos, categorias, isLoading}) => {
         }
       </main>
       <footer><p>Dino Studio WD</p></footer>
+      <button className="btn-flecha-arriba">
+        <svg xmlns="http://www.w3.org/2000/svg" 
+        height="40px" 
+        viewBox="0 -960 960 960" 
+        width="40px" 
+        fill="white">
+          <path d="M446.67-160v-513l-240 240L160-480l320-320 320 320-46.67 47-240-240v513h-66.66Z"/>
+        </svg>
+      </button>
+      <button className="btn-whatsapp">
+        <img src="./whatsapplogo.webp" alt='logo' />
+      </button>
     </div>
   )
 };
