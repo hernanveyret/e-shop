db01d5de4df69ba8216d12091a3dd62a70c0e73f
hernanveyret <hernanveyret@hotmail.com>
2025-06-28 14:00:05 -0300

    carrito

--- src/Components/Carrito.jsx
+++ src/Components/Carrito.jsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect} from 'react'
 import './carrito.css'
 
 const Carrito = ({setIsCarrito, setIsHome}) => {
+  const [ productosCarrito, setProductosCarrito ] = useState([])
   return (
-    <div className="container-carrito">
-      <h4 style={{color:'grey'}}>Tu carrito esta vacio</h4>
-     <main>
-      <p>Productos en tu carrito</p>
-     </main>
+    <div className="container-carrito">     
+      {
+        productosCarrito.length < 0 ? 
+          <p>Tus productos</p>
+        :
+        <img src="./img/carritoVacio.webp" alto="Logo carrito vacio" />
+      }
     </div>
   )
 };
